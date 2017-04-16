befcc05287eae387e389b5c89e02fd6c887dc045
André Santos <afsdab@gmail.com>
2017-04-16 15:14:14 -0300

    bugfix testScreenshots
    - removed required hubstaff.php
    - closing tag omitted

--- tests/screenshots.php
+++ tests/screenshots.php
@@ -1,25 +1,32 @@
-<?php 
-	class test_screenshots extends \PHPUnit_Framework_TestCase
-	{
-		public $stub;
-		public $options = array();
-		function __construct()
-		{
-			$this->options["users"] = "61188";
-			$this->options["projects"] ="112761";
-			$this->options["organizations"] = "27572";
+<?php
 
-			require_once("hubstaff.php");
-	    $this->stub = $this->getMockBuilder('hubstaff\Client')->disableOriginalConstructor()->getMock();
-		}		
-		public function testScreenshots()
-		{
-      $starttime = "2016-03-14";
-      $stoptime = "2016-03-20";
+class test_screenshots extends \PHPUnit_Framework_TestCase
+{
+    public $stub;
+    public $options = [];
 
-	    $this->stub->method('screenshots')->willReturn(json_decode('{"screenshots":[]}',true));	
-	    $this->assertArrayHasKey("screenshots", $this->stub->screenshots($starttime, $stoptime, $this->options, 0));
-		}
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->options["users"] = "61188";
+        $this->options["projects"] = "112761";
+        $this->options["organizations"] = "27572";
 
-?>
+        $this->stub = $this->getMockBuilder('Hubstaff\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+    }
+
+    public function testScreenshots()
+    {
+        $starttime = "2016-03-14";
+        $stoptime = "2016-03-20";
+
+        $this->stub->expects($this->any())
+            ->method('screenshots')
+            ->will($this->returnValue(json_decode('{"screenshots":[]}', true)));
+
+        $this->assertArrayHasKey("screenshots", $this->stub->screenshots($starttime, $stoptime, $this->options, 0));
+    }
+}
