30feb00bb27759208ee49d4d7231e57e15af2dce
Jefersson Nathan <admin@phpse.net>
2017-05-10 17:57:20 -0300

    refactoring tests

--- tests/CustomTest.php
+++ tests/CustomTest.php
@@ -12,97 +12,82 @@ use PHPUnit_Framework_TestCase;
  */
 final class CustomTest extends PHPUnit_Framework_TestCase
 {
-    public function test_custom_report_organizations()
+    /**
+     * @var ClientInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $client;
+
+    /**
+     * @var DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $decoder;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
     {
-        $organizations = uniqid('organization', true);
+        $this->client = $this->createMock(ClientInterface::class);
+        $this->decoder = $this->createMock(DecodeDataInterface::class);
+    }
+
+    /**
+     * @test
+     * @dataProvider provider_valid_options
+     */
+    public function it_should_configure_field_and_parameters(array $options)
+    {
         $authToken = uniqid('authToken', true);
         $appToken = uniqid('appToken', true);
         $startDate = uniqid('startDate', true);
         $endDate = uniqid('endToken', true);
         $url = uniqid('url', true);
 
-        $options = [
-            'organizations' => $organizations,
-        ];
-
+        $optionName = key($options);
         $parameters = [
-            'organizations' => '',
+            $optionName => '',
             'Auth-Token'    => 'header',
             'App-token'     => 'header',
             'start_date'    => '',
             'end_date'      => '',
         ];
 
-        $fields = [
-            'Auth-Token'    => $authToken,
-            'start_date'    => $startDate,
-            'end_date'      => $endDate,
-            'organizations' => $organizations,
-            'App-token'     => $appToken,
-        ];
+        $fields = array_merge(
+            [
+                'Auth-Token'    => $authToken,
+                'start_date'    => $startDate,
+                'end_date'      => $endDate,
+                'App-token'     => $appToken,
+            ],
+            $options
+        );
 
-        /* @var $client ClientInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $client = $this->createMock(ClientInterface::class);
-        $client->expects(self::once())
+        $this->client->expects(self::once())
             ->method('send')
             ->with($fields, $parameters, $url, 0)
             ->will(self::returnValue([]));
 
-        /* @var $decoder DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $decoder = $this->createMock(DecodeDataInterface::class);
-        $decoder->expects(self::once())->method('decode');
+        $this->decoder->expects(self::once())->method('decode');
 
-        $custom = new Custom($client, $decoder);
+        $custom = new Custom($this->client, $this->decoder);
         $custom->customReport($authToken, $appToken, $startDate, $endDate, $options, $url);
     }
 
-    /**
-     * @test
-     */
-    public function it_should_pass_configured_field_and_parameters_for_project_options()
+    public function provider_valid_options()
     {
-        $projects = uniqid('projects', true);
-        $authToken = uniqid('authToken', true);
-        $appToken = uniqid('appToken', true);
-        $startDate = uniqid('startDate', true);
-        $endDate = uniqid('endToken', true);
-        $url = uniqid('url', true);
-
-        $options = [
-            'projects' => $projects,
+        return [
+            [
+                [
+                    'organizations' => uniqid('organization', true),
+                ],
+            ],
+            [
+                [
+                    'projects' => uniqid('projects', true),
+                ],
+            ],
         ];
-
-        $parameters = [
-            'projects'   => '',
-            'Auth-Token' => 'header',
-            'App-token'  => 'header',
-            'start_date' => '',
-            'end_date'   => '',
-        ];
-
-        $fields = [
-            'Auth-Token' => $authToken,
-            'start_date' => $startDate,
-            'end_date'   => $endDate,
-            'projects'   => $projects,
-            'App-token'  => $appToken,
-        ];
-
-        /* @var $client ClientInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $client = $this->createMock(ClientInterface::class);
-        $client->expects(self::once())
-            ->method('send')
-            ->with($fields, $parameters, $url, 0)
-            ->will(self::returnValue([]));
-
-        /* @var $decoder DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $decoder = $this->createMock(DecodeDataInterface::class);
-        $decoder->expects(self::once())->method('decode');
-
-        $custom = new Custom($client, $decoder);
-        $custom->customReport($authToken, $appToken, $startDate, $endDate, $options, $url);
     }
-
 
     /**
      * @todo move it to abstract resource
@@ -111,15 +96,11 @@ final class CustomTest extends PHPUnit_Framework_TestCase
     {
         $expected = '{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}';
 
-        /* @var $client ClientInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $client = $this->createMock(ClientInterface::class);
-        $client->expects(self::once())->method('send')->will(self::returnValue($expected));
+        $this->client->expects(self::once())->method('send')->will(self::returnValue($expected));
 
-        /* @var $decoder DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $decoder = $this->createMock(DecodeDataInterface::class);
-        $decoder->expects(self::once())->method('decode')->will(self::returnValue(json_decode($expected, true)));
+        $this->decoder->expects(self::once())->method('decode')->will(self::returnValue(json_decode($expected, true)));
 
-        $custom = new Custom($client, $decoder);
+        $custom = new Custom($this->client, $this->decoder);
 
         $actual = $custom->customReport(
             'auth_token',
