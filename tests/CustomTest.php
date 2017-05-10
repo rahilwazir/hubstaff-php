dc74f09b0307c461212e40a10f7f1cfa79993cfb
André Santos <andre@dexdev.com.br>
2017-05-10 17:24:26 -0300

    Removed unnecessary methods and created test_custom_report_organizations

--- tests/CustomTest.php
+++ tests/CustomTest.php
@@ -28,12 +28,50 @@ final class CustomTest extends PHPUnit_Framework_TestCase
         $this->options['show_notes'] = '1';
         $this->options['show_activity'] = '1';
         $this->options['include_archived'] = '1';
-
-
-        $this->stub = $this->getMockBuilder('Hubstaff\Client')
-            ->disableOriginalConstructor()
-            ->getMock();
     }
+
+
+    public function test_custom_report_organizations()
+    {
+        $organizations = uniqid('organization', true);
+        $authToken = uniqid('authToken', true);
+        $appToken = uniqid('appToken', true);
+        $startDate = uniqid('startDate', true);
+        $endDate = uniqid('endToken', true);
+        $url = uniqid('url', true);
+
+        $options['organizations'] = $organizations;
+        $parameters = [
+            'organizations' => '',
+            'Auth-Token'    => 'header',
+            'App-token'     => 'header',
+            'start_date'    => '',
+            'end_date'      => '',
+        ];
+
+        $fields = [
+            'Auth-Token'    => $authToken,
+            'start_date'    => $startDate,
+            'end_date'      => $endDate,
+            'organizations' => $organizations,
+            'App-token'     => $appToken,
+        ];
+
+        /* @var $client RequestInterface|\PHPUnit_Framework_MockObject_MockObject */
+        $client = $this->createMock(RequestInterface::class);
+        $client->expects(self::once())
+            ->method('send')
+            ->with($fields, $parameters, $url, 0)
+            ->will(self::returnValue([]));
+
+        /* @var $decoder DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject */
+        $decoder = $this->createMock(DecodeDataInterface::class);
+        $decoder->expects(self::once())->method('decode');
+
+        $custom = new Custom($client, $decoder);
+        $custom->customReport($authToken, $appToken, $startDate, $endDate, $options, $url);
+    }
+
 
     /**
      * @todo move it to abstract resource
@@ -43,14 +81,14 @@ final class CustomTest extends PHPUnit_Framework_TestCase
         $expected = '{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}';
 
         /* @var $client RequestInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $client = $this->getMock(RequestInterface::class);
+        $client = $this->createMock(RequestInterface::class);
         $client->expects(self::once())->method('send')->will(self::returnValue($expected));
 
         /* @var $decoder DecodeDataInterface|\PHPUnit_Framework_MockObject_MockObject */
-        $decoder = $this->getMock(DecodeDataInterface::class);
+        $decoder = $this->createMock(DecodeDataInterface::class);
         $decoder->expects(self::once())->method('decode')->will(self::returnValue(json_decode($expected, true)));
 
-        $custom  = new Custom($client, $decoder);
+        $custom = new Custom($client, $decoder);
 
         $actual = $custom->customReport(
             'auth_token',
@@ -62,55 +100,5 @@ final class CustomTest extends PHPUnit_Framework_TestCase
         );
 
         self::assertEquals(json_decode($expected, true), $actual);
-    }
-
-    public function test_custom_date_my()
-    {
-        $expected = json_decode('{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}', true);
-        $this->stub->expects($this->any())
-            ->method('customDateMy')
-            ->will($this->returnValue($expected));
-
-        $this->assertArrayHasKey('organizations', $this->stub->customDateMy($this->start_date, $this->end_date, $this->options));
-    }
-
-    public function test_custom_member_team()
-    {
-        $expected = json_decode('{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}', true);
-        $this->stub->expects($this->any())
-            ->method('customMemberTeam')
-            ->will($this->returnValue($expected));
-
-        $this->assertArrayHasKey('organizations', $this->stub->customMemberTeam($this->start_date, $this->end_date, $this->options));
-    }
-
-    public function test_custom_member_my()
-    {
-        $expected = json_decode('{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}', true);
-        $this->stub->expects($this->any())
-            ->method('customMemberMy')
-            ->will($this->returnValue($expected));
-
-        $this->assertArrayHasKey('organizations', $this->stub->customMemberMy($this->start_date, $this->end_date, $this->options));
-    }
-
-    public function test_custom_project_team()
-    {
-        $expected = json_decode('{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}', true);
-        $this->stub->expects($this->any())
-            ->method('customProjectTeam')
-            ->will($this->returnValue($expected));
-
-        $this->assertArrayHasKey('organizations', $this->stub->customProjectTeam($this->start_date, $this->end_date, $this->options));
-    }
-
-    public function test_custom_project_my()
-    {
-        $expected = json_decode('{"organizations":[{"id":27572,"name":"Hook Engine","duration":7874,"dates":[{"date":"2016-05-23","duration":7874,"users":[{"id":61188,"name":"Raymond Cudjoe","duration":7874,"projects":[{"id":112761,"name":"Build Ruby Gem","duration":7874}]}]}]}]}', true);
-        $this->stub->expects($this->any())
-            ->method('customProjectMy')
-            ->will($this->returnValue($expected));
-
-        $this->assertArrayHasKey('organizations', $this->stub->customProjectMy($this->start_date, $this->end_date, $this->options));
     }
 }
