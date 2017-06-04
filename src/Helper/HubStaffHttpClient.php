8d6de57a6f71e21f36a0e8bd013102e10b7cd655
André Santos <andre@dexdev.com.br>
2017-06-04 20:25:10 -0300

    HubStaffHttpClientTest and HubStaffHttpClient

---
+++ src/Helper/HubStaffHttpClient.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Hubstaff\Helper;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class HubStaffHttpClient implements ClientInterface
+{
+    private $host;
+    private $method;
+
+    /**
+     * @param string $method
+     * @param string $url
+     * @param array $headers
+     * @param array $parameters
+     *
+     * @return array|string
+     * @throws \RuntimeException
+     * @throws \GuzzleHttp\Exception\RequestException;
+     */
+    public function send($method, $url, $headers, $parameters = [])
+    {
+        $client = new Client(
+            [
+                'base_uri' => $this->host,
+                'headers'  => $headers,
+            ]
+        );
+
+        $this->setMethod($method);
+
+        try {
+            $res = $client->request($this->method, $url, $this->buildOptions($parameters));
+        } catch (RequestException $e) {
+            return $e->getResponse()->getBody()->getContents();
+        }
+
+        return $res->getBody()->getContents();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @param $parameters
+     *
+     * @return array
+     */
+    public function buildOptions($parameters)
+    {
+        if($this->getMethod() == 'GET'){
+            return ['query' => $parameters];
+        }
+
+        if($this->getMethod() == 'POST') {
+            return ['form_params' => $parameters];
+        }
+
+        return [];
+    }
+
+}
